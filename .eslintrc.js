d4e30dbd3b58416ff382e13b6bb46f72b08d3ad3
Frederic Brodbeck <der.froedi@gmail.com>
2020-06-04 15:19:11 +0200

    init

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'commonjs': true,
+		'es6': true
+	},
+	'extends': 'eslint:recommended',
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly',
+		'd3': true,
+		'process': true,
+	},
+	'parserOptions': {
+		'ecmaVersion': 11
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+
+		'no-unused-vars': 'warn',
+	}
+};
