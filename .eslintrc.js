f3b18b04cb9dfab4d6934012ec8b024c10152439
Frederic Brodbeck <der.froedi@gmail.com>
2021-05-18 22:27:23 +0200

    typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,27 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'commonjs': true,
-		'es6': true
+	root: true,
+	parser: '@typescript-eslint/parser',
+	env: {
+		node: true,
+		// browser: true,
+		commonjs: true,
+		es6: true
 	},
-	'extends': 'eslint:recommended',
-	'globals': {
-		'Atomics': 'readonly',
-		'SharedArrayBuffer': 'readonly',
-		'd3': true,
-		'process': true,
-	},
-	'parserOptions': {
-		'ecmaVersion': 11
-	},
-	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		],
+	plugins: [
+		'@typescript-eslint',
+		'jest',
+	],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:jest/recommended',
+	],
+	rules: {
+		indent: ['error', 'tab', { SwitchCase: 1 }],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
 
-		'no-unused-vars': 'warn',
+		'@typescript-eslint/no-var-requires': 0,
 	}
 };
